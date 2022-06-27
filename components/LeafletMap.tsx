1c33926289e71acdbf7e457fc4cea47eec102c66
Jan Pagel <jlp4u@web.de>
2022-06-27 12:16:40 +0200

    adding map with input box

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,18 +1,15 @@
 import { MapContainer, Marker, Popup, TileLayer, useMap, ZoomControl } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-import { height } from "@mui/system";
-import { TextField } from "@mui/material";
 
 export default function LeafletMap() {
 
 
     return (
 
-        <MapContainer  center={[53.6943850, 9.9198327]} zoom={14}  scrollWheelZoom={true} style={{height:'100vh'}} zoomControl={false}>
+        <MapContainer  center={[53.6943850, 9.9198327]} zoom={14}  scrollWheelZoom={true} style={{height:'100vh', width:'100%' ,position: 'fixed'}} zoomControl={false}>
              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
             <ZoomControl position="bottomright" zoomInText="✈️" zoomOutText="🚀"/>
-            
         </MapContainer>
     )
 }
