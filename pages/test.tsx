da5325960309dd8f47ea25f6156e2a084c318845
Jannik Kiel <it1-kielja@student.itech-bs14.de>
2022-06-27 09:39:31 +0200

    Pushy

--- pages/test.tsx
+++ pages/test.tsx
@@ -6,12 +6,50 @@ import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
 import TextField from '@mui/material/TextField'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
 export default function FloatingActionButtons() {
+
+  const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+  
+    const [open, setOpen] = React.useState(false);
+  
+    const handleClick = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpen(false);
+    };
+
+
+
     return (
 <>
+
+
+
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Route wurde berechnet
+        </Alert>
+      </Snackbar>
+
+
 <Box sx={{ '& > :not(style)': { m: 1 } }}>
-      <Fab variant="extended">
+      <Fab variant="extended" onClick={handleClick}>
         <NavigationIcon sx={{ mr: 1 }} />
         Navigate
       </Fab>
