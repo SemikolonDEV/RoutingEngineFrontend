6598e15d116681fabdfd64948cd09479074d2dd5
Jan Pagel <jlp4u@web.de>
2022-06-28 11:41:40 +0200

    test api call

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,11 +1,28 @@
 import { Favorite, Navigation } from "@mui/icons-material"
 import { Box, Fab, Paper, TextField } from "@mui/material"
 import dynamic from "next/dynamic"
-import Head from "next/head"
+import { useRef } from "react";
+import { GraphHopper } from 'graphhopper-js-api-client'
 
 export default function Map() {
 
     const LeafletMap = dynamic(() => import("../components/LeafletMap"), { ssr: false })
+
+    const startDestination = "";
+
+    const targetDestination = "";
+
+    const NavigationClick = () => {
+        console.log(startDestination)
+        const key = "57a00f5e-520b-4b79-b371-5e915498f01b"
+        let ghGeoencoding = new GraphHopper().Geocoding({key: key})
+
+        ghGeoencoding.doRequest({query: startDestination, locale: "de", limit: 1})
+            .then((json : string) => {
+                console.log(json)
+
+            });
+    }
 
     return (
         <>
@@ -19,13 +36,13 @@ export default function Map() {
                 noValidate
                 autoComplete="off"
             >
-                <TextField id="outlined-basic" label="Startpunkt" variant="outlined" />
-                <TextField id="filled-basic" label="Zielort" variant="outlined" />
+                <TextField id="outlined-basic" label="Startpunkt" value={startDestination} variant="outlined" />
+                <TextField id="filled-basic" label="Zielort" value={targetDestination} variant="outlined" />
             </Box>
 
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
                 <Fab variant="extended" >
-                    <Navigation sx={{ mr: 1 }} />
+                    <Navigation sx={{ mr: 1 }} onClick={NavigationClick} />
                     Navigate
                 </Fab>
                 <Fab disabled aria-label="like">
