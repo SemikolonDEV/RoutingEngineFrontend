77a903984c0f7983352c54797ebced738404f496
Jan Pagel <jlp4u@web.de>
2022-06-28 11:53:39 +0200

    fixing ref values

--- pages/map.tsx
+++ pages/map.tsx
@@ -2,22 +2,22 @@ import { Favorite, Navigation } from "@mui/icons-material"
 import { Box, Fab, Paper, TextField } from "@mui/material"
 import dynamic from "next/dynamic"
 import { useRef } from "react";
-import { GraphHopper } from 'graphhopper-js-api-client'
+require('graphhopper-js-api-client')
 
 export default function Map() {
 
     const LeafletMap = dynamic(() => import("../components/LeafletMap"), { ssr: false })
 
-    const startDestination = "";
+    const startDestination = useRef();
 
-    const targetDestination = "";
+    const targetDestination = useRef();
 
     const NavigationClick = () => {
-        console.log(startDestination)
+        console.log(startDestination.current.value)
         const key = "57a00f5e-520b-4b79-b371-5e915498f01b"
         let ghGeoencoding = new GraphHopper().Geocoding({key: key})
 
-        ghGeoencoding.doRequest({query: startDestination, locale: "de", limit: 1})
+        ghGeoencoding.doRequest({query: startDestination.current.value, locale: "de", limit: 1})
             .then((json : string) => {
                 console.log(json)
 
@@ -36,13 +36,13 @@ export default function Map() {
                 noValidate
                 autoComplete="off"
             >
-                <TextField id="outlined-basic" label="Startpunkt" value={startDestination} variant="outlined" />
-                <TextField id="filled-basic" label="Zielort" value={targetDestination} variant="outlined" />
+                <TextField id="outlined-basic" label="Startpunkt" inputRef={startDestination} variant="outlined" />
+                <TextField id="filled-basic" label="Zielort" inputRef={targetDestination} variant="outlined" />
             </Box>
 
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab variant="extended" >
-                    <Navigation sx={{ mr: 1 }} onClick={NavigationClick} />
+                <Fab variant="extended" onClick={NavigationClick}>
+                    <Navigation sx={{ mr: 1 }} />
                     Navigate
                 </Fab>
                 <Fab disabled aria-label="like">
