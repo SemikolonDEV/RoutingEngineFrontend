fe5e6dcf0ab2bca93bb0c664ef9b3ff2b60ae5b0
Lars Unruh <it1-unrula@student.itech-bs14.de>
2022-07-01 11:52:14 +0200

    Kontaktformular

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,17 +1,42 @@
-export default function Kontakt (){
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-    return <>
-    
-    
-    
-    
-    
-    
-    
-    </>
-    
-    
-    
-    
-    
-    }
+export default function MultilineTextFields() {
+    const [value, setValue] = React.useState('Geben Sie hier ihre Nachricht ein');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+    };
+
+    return (
+
+        <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '90ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+
+
+  <div>
+
+  <TextField
+          id="outlined-textarea"
+          label="Kontaktformular"
+          placeholder="Ihre Nachricht hier"
+          multiline
+          maxRows={10}
+   
+
+    />
+
+    </div >
+
+    </Box>
+
+  );
+
+ }
