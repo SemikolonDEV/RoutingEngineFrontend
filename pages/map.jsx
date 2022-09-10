13796eccc933186aa3391d2c31e31c1adf783ea2
Jan-Lukas Pagel <jlp4u@web.de>
2022-09-10 17:19:36 +0200

    add price and logging

--- pages/map.jsx
+++ pages/map.jsx
@@ -1,5 +1,5 @@
-import { Favorite, Navigation } from "@mui/icons-material"
-import { Alert, Box, Fab, Paper, Snackbar, TextField } from "@mui/material"
+import { Favorite, Navigation, RestoreOutlined } from "@mui/icons-material"
+import { Alert, Box, Fab, Paper, Snackbar, TextField, Typography } from "@mui/material"
 import dynamic from "next/dynamic"
 import { useRef, useState , useCallback} from "react";
 import * as React from 'react';
@@ -11,13 +11,15 @@ export default function Map() {
 
     const LeafletMap = dynamic(() => import("../components/LeafletMap"), { ssr: false })
 
-    const startDestination = useRef();
+    const origin = useRef();
 
-    const targetDestination = useRef();
+    const destination = useRef();
 
     const [startCoord, setStartCoord] = useState();
 
     const [targetCoord, setTargetCoord] = useState();
+
+    const [price, setPrice] = useState(null);
 
     const [open, setOpen] = React.useState(false);
 
@@ -35,8 +37,8 @@ export default function Map() {
         const key = "55f4d516-09b7-4bc0-bc8a-76a07455b883"
         let ghGeoencoding = new GraphHopper.Geocoding({key: key})
 
-        const startJson = await ghGeoencoding.doRequest({query: startDestination.current?.value, locale: "de", limit: 1})
-        const targetJson = await ghGeoencoding.doRequest({query: targetDestination.current?.value, locale: "de", limit: 1})
+        const startJson = await ghGeoencoding.doRequest({query: origin.current?.value, locale: "de", limit: 1})
+        const targetJson = await ghGeoencoding.doRequest({query: destination.current?.value, locale: "de", limit: 1})
 
         const startPoint  = startJson.hits[0].point
         const targetPoint = targetJson.hits[0].point
@@ -50,7 +52,31 @@ export default function Map() {
 
         const result = await ghRouting.doRequest(routingRequestdata)
 
+        const minutes = Math.ceil(result.paths[0].time / 60000)
+
+        const price = Math.round(((minutes * 0.15 + 1) + Number.EPSILON) *100) / 100;
+
+        setPrice(price)
+
         setOpen(true)
+
+        // Send Log to Backend
+        fetch("http://localhost:5257/api/Logging", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({
+                origin: origin.current?.value,
+                destination: destination.current?.value,
+                distance: result.paths[0].distance,
+                price: price,
+                travelTime: minutes
+            })
+        }).then(response => console.log(response.json()))
+
+
+
     }
 
     return (
@@ -70,8 +96,8 @@ export default function Map() {
                         autoComplete="off"
                         onSubmit={NavigationClick}
                     >
-                        <TextField id="outlined-basic" label="Startpunkt" required inputRef={startDestination} variant="outlined" />
-                        <TextField id="filled-basic" label="Zielort" required inputRef={targetDestination} variant="outlined" />
+                        <TextField id="outlined-basic" label="Startpunkt" required inputRef={origin} variant="outlined" />
+                        <TextField id="filled-basic" label="Zielort" required inputRef={destination} variant="outlined" />
                         <Box sx={{ '& > :not(style)': { m: 1 } }}>
                             <Fab variant="extended" type="submit">
                                 <Navigation sx={{ mr: 1 }} />
@@ -81,6 +107,7 @@ export default function Map() {
                                 <Favorite />
                             </Fab>
                         </Box>
+                        {price != null &&  <Typography>Der Fahrpreis beträgt: {price.toFixed(2).toString().replace(".", ",")} €</Typography>}
                     </Box>
                 </Paper>
                 <LeafletMap startCoord={startCoord} targetCoord={targetCoord}/>
